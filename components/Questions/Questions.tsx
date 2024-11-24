80262064c0d5c01ce89e6335ca6c16d6714e2571
MarcoLIU27 <lhy1421381989@gmail.com>
2024-11-23 20:08:15 -0800

    feat: questionnare pages

--- components/Questions/Questions.tsx
+++ components/Questions/Questions.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { useState } from 'react';
-import { Button, Group, MultiSelect, Paper, Stepper, Text, Title } from '@mantine/core';
-
+import { Button, Group, Paper, Stepper, Text, Title } from '@mantine/core';
+import classes from './Questions.module.css';
 const genres = [
   { value: 'action', label: 'Action 🥊' },
   { value: 'adventure', label: 'Adventure 🗺️' },
@@ -25,6 +25,12 @@ const genres = [
   { value: 'western', label: 'Western 🤠' },
 ];
 
+const languages = [
+  { value: 'en', label: 'English' },
+  { value: 'zh', label: 'Chinese' },
+  { value: 'jp', label: 'Janpanese' },
+];
+
 function Questions() {
   const [active, setActive] = useState(0);
   const nextStep = () => setActive((current) => (current < 3 ? current + 1 : current));
@@ -32,6 +38,7 @@ function Questions() {
 
   const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
   const [selectedDislikedGenres, setSelectedDislikedGenres] = useState<string[]>([]);
+  const [selectedLanguages, setSelectedLanguages] = useState<string[]>([]);
 
   return (
     <>
@@ -45,34 +52,67 @@ function Questions() {
           position: 'relative',
         }}
       >
-        <Stepper active={active} onStepClick={setActive} allowNextStepsSelect={false} mt="5vh">
+        <Stepper color="pink" active={active} onStepClick={setActive} allowNextStepsSelect={false} mt="5vh" style={{ width: '80%'}}>
           <Stepper.Step label="Genres" description="Likes">
-            <GenreButtonSelector
+            <GenresSelector
               selectedGenres={selectedGenres}
               setSelectedGenres={setSelectedGenres}
             />
           </Stepper.Step>
           <Stepper.Step label="Genres" description="Dislikes">
-            Question 2 content: Verify email
+            <DislikedGenresSelector
+              selectedDislikedGenres={selectedDislikedGenres}
+              setSelectedDislikedGenres={setSelectedDislikedGenres}
+              selectedGenres={selectedGenres}
+            />
           </Stepper.Step>
-          <Stepper.Step label="Language">Question 3 content: Get full access</Stepper.Step>
+          <Stepper.Step label="Language">
+            <LanguagesSelector
+              selectedLanguages={selectedLanguages}
+              setSelectedLanguages={setSelectedLanguages}
+            />
+          </Stepper.Step>
+          <Stepper.Step label="Language">
+            Question 4 content: Get full access
+          </Stepper.Step>
+          <Stepper.Step label="Language">
+            Question 5 content: Get full access
+          </Stepper.Step>
+          <Stepper.Step label="Language">
+            Question 6 content: Get full access
+          </Stepper.Step>
           <Stepper.Completed>
             Completed, click back button to get to previous step
           </Stepper.Completed>
         </Stepper>
 
-        <Group justify="center" mt="xl">
-          <Button variant="default" onClick={prevStep}>
+        <Group justify="center" mt="xl" gap="2rem">
+          <Button
+            className={classes.button}
+            size="lg"
+            radius="xl"
+            variant="outline"
+            color="pink"
+            onClick={prevStep}
+          >
             Back
           </Button>
-          <Button onClick={nextStep}>Next step</Button>
+          <Button
+            className={classes.button}
+            size="lg"
+            radius="xl"
+            variant="gradient"
+            gradient={{ from: 'pink', to: 'yellow', deg: 60 }}
+            onClick={nextStep}>
+            Next
+          </Button>
         </Group>
       </Paper>
     </>
   );
 }
 
-const GenreButtonSelector = ({ selectedGenres, setSelectedGenres }: any): any => {
+const GenresSelector = ({ selectedGenres, setSelectedGenres }: any): any => {
   const toggleGenre = (value: string) => {
     if (selectedGenres.includes(value)) {
       // Remove if already selected
@@ -84,7 +124,7 @@ const GenreButtonSelector = ({ selectedGenres, setSelectedGenres }: any): any =>
   };
 
   return (
-    <div style={{ maxWidth: 600, margin: '0 auto', padding: '2rem 0' }}>
+    <div className={classes.content}>
       <Title order={2} ta="center" mb="md">
         Select your preferred genres
       </Title>
@@ -92,7 +132,7 @@ const GenreButtonSelector = ({ selectedGenres, setSelectedGenres }: any): any =>
         Pick up to 3 genres that you enjoy the most.
       </Text>
 
-      <Group gap="md" ta="center" wrap="wrap">
+      <Group className={classes.choices}>
         {genres.map((genre) => (
           <Button
             key={genre.value}
@@ -103,6 +143,81 @@ const GenreButtonSelector = ({ selectedGenres, setSelectedGenres }: any): any =>
             size="md"
           >
             {genre.label}
+          </Button>
+        ))}
+      </Group>
+    </div>
+  );
+};
+
+const DislikedGenresSelector = ({ selectedDislikedGenres, setSelectedDislikedGenres, selectedGenres }: any): any => {
+  const toggleGenre = (value: string) => {
+    if (selectedDislikedGenres.includes(value)) {
+      // Remove if already selected
+      setSelectedDislikedGenres(selectedDislikedGenres.filter((genre: any) => genre !== value));
+    } else {
+      setSelectedDislikedGenres([...selectedDislikedGenres, value]);
+    }
+  };
+
+  return (
+    <div className={classes.content}>
+      <Title order={2} ta="center" mb="md">
+        Select your disliked genres
+      </Title>
+      <Text ta="center" mb="sm" color="dimmed">
+        Select all genres that you want to avoid.
+      </Text>
+
+      <Group className={classes.choices}>
+        {genres.map((genre) => (
+          <Button
+            key={genre.value}
+            variant={ selectedDislikedGenres.includes(genre.value) ? 'filled' : 'outline'}
+            color={selectedDislikedGenres.includes(genre.value) ? 'pink' : 'gray'}
+            disabled={selectedGenres.includes(genre.value)}
+            onClick={() => toggleGenre(genre.value)}
+            radius="xl"
+            size="md"
+          >
+            {genre.label}
+          </Button>
+        ))}
+      </Group>
+    </div>
+  );
+};
+
+const LanguagesSelector = ({ selectedLanguages, setSelectedLanguages }: any): any => {
+  const toggleGenre = (value: string) => {
+    if (selectedLanguages.includes(value)) {
+      // Remove if already selected
+      setSelectedLanguages(selectedLanguages.filter((genre: any) => genre !== value));
+    } else {
+      setSelectedLanguages([...selectedLanguages, value]);
+    }
+  };
+
+  return (
+    <div className={classes.content}>
+      <Title order={2} ta="center" mb="md">
+        Select your prefered languages
+      </Title>
+      <Text ta="center" mb="sm" color="dimmed">
+        Pick languages of the movie you like.
+      </Text>
+
+      <Group className={classes.choices}>
+        {languages.map((language) => (
+          <Button
+            key={language.value}
+            variant={ selectedLanguages.includes(language.value) ? 'filled' : 'outline'}
+            color={selectedLanguages.includes(language.value) ? 'pink' : 'gray'}
+            onClick={() => toggleGenre(language.value)}
+            radius="xl"
+            size="md"
+          >
+            {language.label}
           </Button>
         ))}
       </Group>
